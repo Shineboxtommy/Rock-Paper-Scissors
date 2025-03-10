069364ee996863af6aadfa7effa21f29d46910c6
shineboxtommy <176469171+Shineboxtommy@users.noreply.github.com>
2025-03-10 00:58:04 -0500

    Added the playGame function

--- script.js
+++ script.js
@@ -3,28 +3,24 @@
 let humanScore = 0;
 let computerScore = 0;
 
-
-
-
-
 //Create Function named Get Computer Choice
 
 let getComputerChoice = () => 
 {
     let randomNumber = Math.floor(Math.random() * 3) // Returns a value of 0, 1, or 2
-    console.log(randomNumber);
+    //console.log(randomNumber);
     return  randomNumber === 0 ? 'rock'
     : randomNumber === 1 ? 'paper'
     : randomNumber === 2 ? 'scissors'
     : 'Invalid Choice';
 };
 
-console.log(getComputerChoice()); // Returns 'Rock', 'Paper', or 'Scissors'
+//console.log(getComputerChoice()); // Returns 'Rock', 'Paper', or 'Scissors'
 
 let getHumanChoice = () => 
 {
     let humanChoice = prompt('Choose Rock, Paper, or Scissors').toLowerCase();
-    console.log(humanChoice);
+    return humanChoice;
 }
 
 
@@ -55,3 +51,28 @@ let playRound = (humanChoice, computerChoice) =>
         computerScore++;
     }
 }
+
+let playGame = () =>
+{
+    for (let i = 0; i < 5; i++)
+    {
+        let humanChoice = getHumanChoice();
+        console.log(humanChoice);
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    if (humanScore > computerScore)
+    {
+        console.log(`You win! ${humanScore} to ${computerScore}`);
+    }
+    else if (humanScore === computerScore)
+    {
+        console.log(`It's a tie! ${humanScore} to ${computerScore}`);
+    }
+    else
+    {
+        console.log(`You lose! ${computerScore} to ${humanScore}`);
+    }
+}
+
+playGame();
