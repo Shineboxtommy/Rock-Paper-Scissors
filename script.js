f49f3ec8db8a51e3c40f80e9d5c4e94e274b4cfc
= <=>
2025-03-29 23:46:31 -0500

    add js code

--- script.js
+++ script.js
@@ -3,6 +3,67 @@
 let humanScore = 0;
 let computerScore = 0;
 
+//Reference to the body of the page
+const body = document.querySelector("body");
+const button1 = document.createElement("button");
+const button2 = document.createElement("button");
+const button3 = document.createElement("button");
+
+button1.innerText = "Rock";
+button2.innerText = "Paper";
+button3.innerText = "Scissors";
+
+const buttonContainer = document.createElement("div");
+
+body.appendChild(buttonContainer);
+buttonContainer.appendChild(button1);
+buttonContainer.appendChild(button2);
+buttonContainer.appendChild(button3);
+
+//Div to display results
+const resultsDiv = document.createElement("div");
+body.appendChild(resultsDiv);
+const playerDiv = document.createElement("div");
+const computerDiv = document.createElement("div");
+const playerChoice = document.createElement("h2");
+const playerScore = document.createElement("p");
+const computerChoice = document.createElement("h2");
+const compScore = document.createElement("p");
+const computerChoiceText = document.createElement("p");
+resultsDiv.appendChild(playerDiv);
+resultsDiv.appendChild(computerDiv);
+playerDiv.appendChild(playerChoice);
+computerDiv.appendChild(computerChoice);
+playerDiv.appendChild(playerScore);
+computerDiv.appendChild(compScore);
+computerDiv.appendChild(computerChoiceText);
+
+
+body.style.display = "flex";
+body.style.flexDirection = "column";
+buttonContainer.style.alignSelf = "center";
+buttonContainer.style.marginTop = "5rem";
+buttonContainer.style.marginBottom = "5rem";
+/* Results Div Styling */
+resultsDiv.style.backgroundColor = "grey";
+resultsDiv.style.width = "50%";
+resultsDiv.style.margin = "auto";
+playerChoice.innerText = "Player";
+computerChoice.innerText = "Computer";
+playerScore.innerText = 0;
+compScore.innerText = 0;
+//computerChoiceText.innerText = "Rock";
+playerDiv.style.display = "flex";
+playerDiv.style.width = "20rem";
+playerDiv.style.alignItems = "center";
+playerDiv.style.gap = "3rem";
+playerDiv.style.justifyContent = "space-evenly";
+computerDiv.style.display = "flex";
+computerDiv.style.gap = "3rem";
+computerDiv.style.alignItems = "center";
+computerDiv.style.justifyContent = "space-evenly";
+computerDiv.style.width = "25rem";
+
 //Create Function named Get Computer Choice
 
 let getComputerChoice = () => 
@@ -49,7 +110,30 @@ let playRound = (humanChoice, computerChoice) =>
     {
         console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
         computerScore++;
+
     }
+    console.log(`Human Score: ${humanScore}`);
+    playerScore.innerText = humanScore;
+    compScore.innerText = computerScore;
+    computerChoiceText.innerText = computerChoice;
+    
+    if (humanScore === 5)
+    {
+        alert("You Win!");
+        restartGame();
+    }
+    if (computerScore === 5)
+    {
+        alert("You Lose!");
+        restartGame();
+    }
+
+}
+
+let restartGame = () =>
+{
+    humanScore = 0;
+    computerScore = 0;
 }
 
 // let playGame = () =>
@@ -74,19 +158,6 @@ let playRound = (humanChoice, computerChoice) =>
 //     }
 // }
 
-//Reference to the body of the page
-const body = document.querySelector("body");
-const button1 = document.createElement("button");
-const button2 = document.createElement("button");
-const button3 = document.createElement("button");
-
-button1.innerText = "Rock";
-button2.innerText = "Paper";
-button3.innerText = "Scissors";
-
-body.appendChild(button1);
-body.appendChild(button2);
-body.appendChild(button3);
 
 
 // button1.addEventListener("click", function() {
@@ -106,14 +177,16 @@ body.appendChild(button3);
 //     playRound(humanChoice, getComputerChoice());
 // });
 
-const buttons = [button1, button2, button3];
+let buttons = [button1, button2, button3];
 
 buttons.forEach(button => {
     button.addEventListener("click", function() {
             //Get the buttons text
     humanChoice = button.innerText.toLowerCase();
     playRound(humanChoice, getComputerChoice());
+    
     });
+    
 });
 
 //playGame();
